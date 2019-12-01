4fea562405f102cc570b547bcea7c430717c609f
nguyenkhacminh181@gmail.com <nguyenkhacminh181@gmail.com>
2019-12-01 11:58:45 +0700

    homework 1

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
+import random
+
 playerX = 3
 playerY = 3
+
+enemyX = random.randint(0, 6)
+enemyY = random.randint(0, 6)
 
 def printMap(side):
     for i in range(side):
         for j in range(side):
             if i == playerY and j == playerX:
                 print(" P ", end="")
+            elif i == enemyY and j == enemyX:
+                print(" E ", end="")    
             else:
                 print(" * ", end="")
-
         print()
 
-
-
-
 def run():
-  global playerX, playerY
+  global playerX, playerY, enemyX
   # Lay lenh cua ng dung tu console
   direct = input("Your move: ")
   # check lenh va xu ly logic
@@ -27,12 +30,13 @@ def run():
     playerX += 1
   if direct.lower() == "left":
     playerX -= 1  
+    
+  enemyX += 1
 
-printMap(7)
+  if playerX == enemyX and playerY == enemyY: 
+    exit(0)
 
 while(True):
-  #xu ly logic
   print(chr(27) + "[2J")
   printMap(7)
   run()
-  #in lai map
