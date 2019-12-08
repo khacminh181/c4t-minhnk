d6487c2e7c49d36327519fbee3430c187fa3eeea
nguyenkhacminh181@gmail.com <nguyenkhacminh181@gmail.com>
2019-12-08 10:46:20 +0700

    session 3

--- session2/fibonaci.py
+++ session2/fibonaci.py
@@ -19,3 +19,18 @@ def fibonacci_recursion(n):
     return fibonacci_recursion(n-1) + fibonacci_recursion(n-2)
 
 print(fibonacci_recursion(4))
+
+
+cache = {}
+def fibonacci_cache(n):
+  if (n in cache):
+    return cache[n]
+  elif (n < 2): 
+    return n
+  else:
+    result = fibonacci_cache(n - 1) + fibonacci_cache(n - 2)
+    cache[n] = result
+    print(cache)
+    return result
+
+print(fibonacci_cache(4))
